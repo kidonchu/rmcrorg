335f7d102d94f07086d5585ffd894fe1f041286b
kidonchu <kidonchu@gmail.com>
2014-06-27 17:51:23 -0500

    Add bootstrap

--- wp-content/themes/rmcr/functions.php
+++ wp-content/themes/rmcr/functions.php
@@ -8,65 +8,65 @@
 /**
  * Set the content width based on the theme's design and stylesheet.
  */
-if ( ! isset( $content_width ) ) {
+if ( !isset( $content_width ) ) {
 	$content_width = 640; /* pixels */
 }
 
-if ( ! function_exists( 'rmcr_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function rmcr_setup() {
-
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on rmcr, use a find and replace
-	 * to change 'rmcr' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'rmcr', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+if ( !function_exists( 'rmcr_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	function rmcr_setup() {
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'rmcr' ),
-	) );
-	
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-	) );
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on rmcr, use a find and replace
+		 * to change 'rmcr' to the name of your theme in all the template files
+		 */
+		load_theme_textdomain( 'rmcr', get_template_directory() . '/languages' );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link'
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	// Setup the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'rmcr_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-}
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+		 */
+		//add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'primary' => __( 'Primary Menu', 'rmcr' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+		) );
+
+		/*
+		 * Enable support for Post Formats.
+		 * See http://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support( 'post-formats', array(
+			'aside', 'image', 'video', 'quote', 'link'
+		) );
+
+		// Setup the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'rmcr_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+	}
 endif; // rmcr_setup
 add_action( 'after_setup_theme', 'rmcr_setup' );
 
@@ -86,22 +86,26 @@ function rmcr_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 }
+
 add_action( 'widgets_init', 'rmcr_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
 function rmcr_scripts() {
-	wp_enqueue_style( 'rmcr-style', get_stylesheet_uri() );
+
+	wp_enqueue_style( 'rmcr-bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '3.1.1' );
+	wp_enqueue_style( 'rmcr-style', get_stylesheet_uri(), array( 'rmcr-bootstrap' ) );
 
 	wp_enqueue_script( 'rmcr-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'rmcr-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'rmcr-bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '3.1.1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
+
 add_action( 'wp_enqueue_scripts', 'rmcr_scripts' );
 
 /**
