05514b9b6ddd1e8110be0db5a819204b22776ea0
kidonchu <kidonchu@gmail.com>
2014-06-27 20:21:30 -0500

    Add custom post type: Dog

--- wp-content/plugins/rmcr-plugins/rmcr-functions.php
+++ wp-content/plugins/rmcr-plugins/rmcr-functions.php
@@ -4,4 +4,36 @@ Plugin Name: Functions Plugin for RMCR
 Description: Site specific functions.php for RMCR
 */
 
-
+/* Custom Post Type: Dog */
+add_action( 'init', 'register_custom_post_type_dog' );
+function register_custom_post_type_dog() {
+	register_post_type( 'dog', array(
+		'label'           => 'Dogs',
+		'description'     => 'Custom Post Type: Dog',
+		'public'          => true,
+		'show_ui'         => true,
+		'show_in_menu'    => true,
+		'capability_type' => 'post',
+		'map_meta_cap'    => true,
+		'hierarchical'    => false,
+		'rewrite'         => array( 'slug' => 'dogs', 'with_front' => true ),
+		'query_var'       => true,
+		'supports'        => array( 'title', 'excerpt', 'trackbacks', 'custom-fields', 'revisions', 'thumbnail', 'page-attributes', 'post-formats', 'custom-post-templates' ),
+		'labels'          => array(
+			'name'               => 'Dogs',
+			'singular_name'      => 'Dog',
+			'menu_name'          => 'Dogs',
+			'add_new'            => 'Add Dog',
+			'add_new_item'       => 'Add New Dog',
+			'edit'               => 'Edit',
+			'edit_item'          => 'Edit Dog',
+			'new_item'           => 'New Dog',
+			'view'               => 'View Dog',
+			'view_item'          => 'View Dog',
+			'search_items'       => 'Search Dogs',
+			'not_found'          => 'No Dogs Found',
+			'not_found_in_trash' => 'No Dogs Found in Trash',
+			'parent'             => 'Parent Dog',
+		)
+	) );
+}
