ccb8598efd0c3b08bc73871a587f06191d599c37
kidonchu <kidonchu@gmail.com>
2014-06-27 21:11:11 -0500

    Add template for single-dog

---
+++ wp-content/themes/rmcr/single-dog.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name Posts: Dog
+ * The Template for displaying all single posts.
+ *
+ * @package rmcr
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="row">
+
+	<div id="primary" class="col-md-9" style="background-color: yellow">
+
+		<main id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'partials/content', 'single-dog' ); ?>
+
+				<?php rmcr_post_nav(); ?>
+
+			<?php endwhile; // end of the loop ?>
+
+		</main><!-- #main -->
+
+	</div><!-- #primary -->
+
+	<?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer(); ?>
