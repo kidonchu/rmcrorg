51a6c1ca7fcfcba0b19e1f82c901f2f783fe9ba7
kidonchu <kidonchu@gmail.com>
2014-06-29 19:26:56 -0500

    Add RMCR_Dog class.

---
+++ wp-content/themes/rmcr/inc/classes/rmcr-dog.php
@@ -0,0 +1,87 @@
+<?php
+
+class RMCR_Dog {
+
+	private $_data = array();
+	private $_fields = array(
+		'name', 'status', 'rmcr_id', 'gender', 'age', 'adoption_fee',
+		'short_description', 'description', 'not_good_with',
+		'photo1', 'photo2', 'photo3', 'photo4',
+	);
+
+	public function __construct() {
+		foreach ( $this->_fields as $field ) {
+			$this->setData( $field, get_field( $field ) );
+		}
+	}
+
+	public function getStatusLabel() {
+		$html = '';
+
+		$status = $this->getData( 'status' );
+		if ( $status ) {
+			switch ( $status ) {
+				case 'Pending':
+					$html = '<span class="label label-danger">PENDING</span>';
+					break;
+				case 'Adoptable':
+					$html = '<span class="label label-success">ADOPTABLE</span>';
+					break;
+				case 'Adopted':
+					$html = '<span class="label label-info">ADOPTED</span>';
+					break;
+			}
+		}
+
+		return $html;
+	}
+
+	public function getAgeTranslated() {
+		$ret = false;
+
+		$age = $this->getData( 'age' );
+		if ( $age ) {
+			switch ( $age ) {
+				case 1:
+				case 2:
+				case 3:
+					$ret = 'Puppy';
+					break;
+				case 4:
+				case 5:
+				case 6:
+					$ret = 'Young';
+					break;
+				case 7:
+				case 9:
+				case 10:
+					$ret = 'Adult';
+					break;
+				default:
+					$ret = 'Senior';
+			}
+		}
+
+		return $ret;
+	}
+
+	public function getAdoptLink()
+	{
+		if ($this->getData('name')) {
+			return '<a href="#" class="btn btn-warning pull-right">Adopt ' . $this->getData('name') . ' Now</a>';
+		}
+		return '';
+	}
+
+	public function setData( $key, $value ) {
+		$this->_data[ $key ] = $value;
+		return $this;
+	}
+
+	public function getData( $key ) {
+		if ( isset( $this->_data[ $key ] ) ) {
+			return $this->_data[ $key ];
+		}
+		return null;
+	}
+}
--- wp-content/themes/rmcr/partials/content-single-dog.php
+++ wp-content/themes/rmcr/partials/content-single-dog.php
@@ -4,123 +4,131 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php $dog = new RMCR_Dog(); ?>
+<?php if ( $dog->getData( 'name' ) ) : ?>
 
-	<header class="entry-header">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<h1 class="entry-title">
-			<?php the_field( 'name' ) ?>
-		</h1>
+		<header class="entry-header">
 
-	</header>
-	<!-- .entry-header -->
+			<h1 class="entry-title">
+				<?php echo $dog->getData( 'name' ) ?>
+			</h1>
 
-	<div class="entry-content">
+		</header>
+		<!-- .entry-header -->
 
-		<div class="row">
+		<div class="entry-content">
 
-			<div class="col-md-6">
-				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-					</ol>
+			<div class="row">
 
-					<!-- Wrapper for slides -->
-					<div class="carousel-inner">
-						<div class="item active">
-							<img src="<?php the_field( 'photo1' ) ?>" alt="">
+				<div class="col-md-6">
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+						</ol>
 
-							<div class="carousel-caption">
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+							<div class="item active">
+								<img src="<?php echo $dog->getData( 'photo1' ) ?>" alt="">
+
+								<div class="carousel-caption">
+								</div>
+							</div>
+							<div class="item">
+								<img src="<?php echo $dog->getData( 'photo2' ) ?>" alt="...">
+
+								<div class="carousel-caption">
+								</div>
+							</div>
+							<div class="item">
+								<img src="<?php echo $dog->getData( 'photo3' ) ?>" alt="...">
+
+								<div class="carousel-caption">
+								</div>
+							</div>
+							<div class="item">
+								<img src="<?php echo $dog->getData( 'photo4' ) ?>" alt="...">
+
+								<div class="carousel-caption">
+								</div>
 							</div>
 						</div>
-						<div class="item">
-							<img src="<?php the_field( 'photo2' ) ?>" alt="...">
 
-							<div class="carousel-caption">
-							</div>
-						</div>
-						<div class="item">
-							<img src="<?php the_field( 'photo3' ) ?>" alt="...">
-
-							<div class="carousel-caption">
-							</div>
-						</div>
-						<div class="item">
-							<img src="<?php the_field( 'photo4' ) ?>" alt="...">
-
-							<div class="carousel-caption">
-							</div>
-						</div>
+						<!-- Controls -->
+						<a class="left carousel-control" href="#carousel-example-generic" role="button"
+						   data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+						</a>
+						<a class="right carousel-control" href="#carousel-example-generic" role="button"
+						   data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+						</a>
 					</div>
 
-					<!-- Controls -->
-					<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left"></span>
-					</a>
-					<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right"></span>
-					</a>
 				</div>
 
+				<div class="col-md-6">
+
+					<h2 class="entry-subtitle">Description</h2>
+
+					<dl class="dl-horizontal">
+						<dt>Status:</dt>
+						<dd><?php echo $dog->getStatusLabel(); ?></dd>
+						<dt>RMCR ID:</dt>
+						<dd><?php echo $dog->getData( 'rmcr_id' ); ?></dd>
+						<dt>Gender:</dt>
+						<dd><?php echo $dog->getData( 'gender' ); ?></dd>
+						<dt>Age:</dt>
+						<dd><?php echo $dog->getAgeTranslated(); ?></dd>
+						<dt>Adoption Fee:</dt>
+						<dd>&dollar;<?php echo $dog->getData( 'adoption_fee' ); ?></dd>
+						<dt>Not Good With:</dt>
+						<dd><?php echo $dog->getData( 'not_good_with' ); ?></dd>
+					</dl>
+
+				</div>
+
+				<?php echo $dog->getAdoptLink() ?>
+
 			</div>
 
-			<div class="col-md-6">
+			<h2 class="entry-subtitle">About <?php the_field( 'name' ); ?></h2>
 
-				<h2 class="entry-subtitle">Description</h2>
+			<p><?php the_field( 'description' ); ?></p>
 
-				<dl class="dl-horizontal">
-					<dt>Status:</dt>
-					<dd><?php echo apply_filters( 'convert_status', get_field( 'status' ) ); ?></dd>
-					<dt>RMCR ID:</dt>
-					<dd><?php the_field( 'rmcr_id' ); ?></dd>
-					<dt>Gender:</dt>
-					<dd><?php the_field( 'gender' ); ?></dd>
-					<dt>Age:</dt>
-					<dd><?php echo apply_filters( 'convert_age', get_field( 'age' ) ); ?></dd>
-					<dt>Adoption Fee:</dt>
-					<dd>&dollar;<?php the_field( 'adoption_fee' ); ?></dd>
-					<dt>Not Good With:</dt>
-					<dd><?php the_field( 'not_good_with' ); ?></dd>
-				</dl>
+			<p>
+				We reserve the right to refuse an adoption to any person for the well-being of the dog.
+			</p>
 
-				<h2 class="entry-subtitle">Interested in <?php the_field( 'name' ) ?>?</h2>
+			<p>
+				Prior to adoption, <?php the_field( 'name' ); ?> will be micro-chipped, neutured and up-to-date on his
+				shots. If you would like to meet Emmitt to see if he is a good fit for your home, please fill out an
+				Adoption Application Online. Emmitt's adoption fee is $<?php the_field( 'adoption_fee' ); ?>.
+			</p>
 
-				<a href="#">Adopt Me</a>
-			</div>
+			<?php echo $dog->getAdoptLink() ?>
 
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'rmcr' ),
+				'after'  => '</div>',
+			) );
+			?>
 		</div>
+		<!-- .entry-content -->
 
-		<h2 class="entry-subtitle">About <?php the_field( 'name' ); ?></h2>
+		<footer class="entry-footer">
+			<?php edit_post_link( __( 'Edit', 'rmcr' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer>
+		<!-- .entry-footer -->
 
-		<p><?php the_field( 'description' ); ?></p>
+	</article><!-- #post-## -->
 
-		<p>
-			We reserve the right to refuse an adoption to any person for the well-being of the dog.
-		</p>
+<?php endif; ?>
 
-		<p>
-			Prior to adoption, <?php the_field( 'name' ); ?> will be micro-chipped, neutured and up-to-date on his
-			shots. If you would like to meet Emmitt to see if he is a good fit for your home, please fill out an
-			Adoption Application Online. Emmitt's adoption fee is $<?php the_field( 'adoption_fee' ); ?>.
-		</p>
-
-
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'rmcr' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div>
-	<!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'rmcr' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer>
-	<!-- .entry-footer -->
-
-</article><!-- #post-## -->
