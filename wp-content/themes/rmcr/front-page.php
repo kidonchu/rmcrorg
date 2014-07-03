092ff32409ed449851a5e3754e72d83ca9a1abf5
kidonchu <kidonchu@gmail.com>
2014-07-03 08:48:54 -0500

    Add carousel and post slider

--- wp-content/themes/rmcr/front-page.php
+++ wp-content/themes/rmcr/front-page.php
@@ -1,46 +1,64 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package rmcr
- */
+<?php get_header(); ?>
 
-get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+	</ol>
 
-		<?php if ( have_posts() ) : ?>
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		<div class="item active">
+			<img src="http://placehold.it/1170x300" alt="">
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="carousel-caption">
+			</div>
+		</div>
+		<div class="item">
+			<img src="http://placehold.it/1170x300" alt="">
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+			<div class="carousel-caption">
+			</div>
+		</div>
+		<div class="item">
+			<img src="http://placehold.it/1170x300" alt="">
 
-			<?php endwhile; ?>
+			<div class="carousel-caption">
+			</div>
+		</div>
+		<div class="item">
+			<img src="http://placehold.it/1170x300" alt="">
 
-			<?php rmcr_paging_nav(); ?>
+			<div class="carousel-caption">
+			</div>
+		</div>
+	</div>
 
-		<?php else : ?>
+	<!-- Controls -->
+	<a class="left carousel-control" href="#carousel-example-generic" role="button"
+	   data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+	</a>
+	<a class="right carousel-control" href="#carousel-example-generic" role="button"
+	   data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+	</a>
+</div>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+<div class="row">
 
-		<?php endif; ?>
+	<div class="col-md-9">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php if ( function_exists( 'rps_show' ) ) echo rps_show( $category_ids, $total_posts, $post_include_ids, $post_exclude_ids ); ?>
 
-<?php get_sidebar(); ?>
+	</div>
+
+	<?php get_sidebar(); ?>
+
+</div>
+
 <?php get_footer(); ?>
