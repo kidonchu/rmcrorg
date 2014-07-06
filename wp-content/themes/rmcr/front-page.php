5e4dc7a71bccba8518b11cf668c5ee5ded5547bf
kidonchu <kidonchu@gmail.com>
2014-07-06 08:35:04 -0500

    Add dog slider to homepage

--- wp-content/themes/rmcr/front-page.php
+++ wp-content/themes/rmcr/front-page.php
@@ -1,59 +1,32 @@
 <?php get_header(); ?>
 
-
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-	</ol>
-
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner">
-		<div class="item active">
-			<img src="http://placehold.it/1170x300" alt="">
-
-			<div class="carousel-caption">
-			</div>
-		</div>
-		<div class="item">
-			<img src="http://placehold.it/1170x300" alt="">
-
-			<div class="carousel-caption">
-			</div>
-		</div>
-		<div class="item">
-			<img src="http://placehold.it/1170x300" alt="">
-
-			<div class="carousel-caption">
-			</div>
-		</div>
-		<div class="item">
-			<img src="http://placehold.it/1170x300" alt="">
-
-			<div class="carousel-caption">
-			</div>
-		</div>
-	</div>
-
-	<!-- Controls -->
-	<a class="left carousel-control" href="#carousel-example-generic" role="button"
-	   data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left"></span>
-	</a>
-	<a class="right carousel-control" href="#carousel-example-generic" role="button"
-	   data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right"></span>
-	</a>
-</div>
-
 <div class="row">
+
+	<div class="hero">
+		<img src="http://placehold.it/1170x300" alt="Rocky Mountain Cocker Rescue Org">
+	</div>
 
 	<div class="col-md-9">
 
-		<?php if ( function_exists( 'rps_show' ) ) echo rps_show( $category_ids, $total_posts, $post_include_ids, $post_exclude_ids ); ?>
+		<div class="dog-slider">
+
+			<header class="clearfix">
+
+				<h2 class="home-title pull-left">
+					Cocker Spaniels Waiting For Your Adoption
+				</h2>
+
+				<a class="btn btn-warning pull-right"
+				   href="<?php echo get_home_url( null, 'dog-adoption/adopt-rescued-cocker-spaniels' ) ?>">
+					View All Adoptable Dogs
+				</a>
+
+			</header>
+
+			<?php if ( class_exists( 'Rds' ) ) : ?>
+				<?php Rds::show_slider() ?>
+			<?php endif; ?>
+		</div>
 
 	</div>
 
