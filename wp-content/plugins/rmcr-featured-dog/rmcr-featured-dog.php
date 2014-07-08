8a0a5aef6a0cdc439b6d76fdf127dc829d4c3862
kidonchu <kidonchu@gmail.com>
2014-07-07 21:54:13 -0500

    Add featured dog widget

--- wp-content/plugins/rmcr-featured-dog/rmcr-featured-dog.php
+++ wp-content/plugins/rmcr-featured-dog/rmcr-featured-dog.php
@@ -9,6 +9,7 @@ Author: Kidon Chu
 define( 'RFD__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 require_once( RFD__PLUGIN_DIR . 'class.rfd.php' );
+require_once( RFD__PLUGIN_DIR . 'class.rfd-widget.php' );
 
 add_action( 'init', array( 'Rfd', 'init' ) );
 
