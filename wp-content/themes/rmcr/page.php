959aed45437d852d20366d85af14c626c684152d
kidonchu <kidonchu@gmail.com>
2014-07-08 20:10:25 -0500

    Add WordPress SEO by Yoast plugin

--- wp-content/themes/rmcr/page.php
+++ wp-content/themes/rmcr/page.php
@@ -1,9 +1,13 @@
 <?php get_header(); ?>
+
 <div class="row">
+
 	<div class="col-md-9">
+
 		<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
 			yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
 		} ?>
+
 		<main id="content" class="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -13,7 +17,11 @@
 			<?php endwhile; // end of the loop. ?>
 
 		</main>
+
 	</div>
+
 	<?php get_sidebar(); ?>
+
 </div>
+
 <?php get_footer(); ?>
