adc0cd0c60ae14f9d07fa466740c95ebbf708e26
kidonchu <kidonchu@gmail.com>
2014-07-16 08:05:35 -0500

    Add WP Simple Paypal Shopping cart plugin

--- wp-content/themes/rmcr/content-page.php
+++ wp-content/themes/rmcr/content-page.php
@@ -8,8 +8,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<img src="http://placehold.it/1000x300" alt="" width="100%"/>
-
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
