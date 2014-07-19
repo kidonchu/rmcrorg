816c1c2fa1d3f6cf08a7e4d568247f56a661ad38
kidonchu <kidonchu@gmail.com>
2014-07-19 00:31:36 -0500

    Add page-products-grid.php

---
+++ wp-content/themes/rmcr/page-products-grid.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template Name: Product Grid
+ * The Template for displaying dog grid.
+ *
+ * @package rmcr
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="row">
+
+	<div class="col-xs-9">
+
+		<main id="main" class="site-main" role="main">
+
+			<?php if ( have_posts() ) : the_post(); ?>
+
+				<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+				} ?>
+
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header>
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+
+			<?php endif; ?>
+
+			<!-- Display grid of dogs -->
+			<?php $wp_query = new WP_Query( array( 'post_type' => 'product', 'posts_per_page' => - 1 ) ); ?>
+			<?php if ( $wp_query->have_posts() ) : ?>
+
+				<div class="row product-grid">
+
+					<?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+
+						<?php $id = get_the_ID();
+						$product = new RMCR_Product( $id ); ?>
+
+						<?php // if status of the dog is specified, filter by status ?>
+						<?php if ( ! $product->get_data( 'status' ) ) : ?>
+							<?php continue; ?>
+						<?php endif; ?>
+
+						<div class="col-md-2">
+							<?php $product->the_product_block(); ?>
+						</div>
+
+					<?php endwhile; ?>
+
+				</div>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'content', 'none' ); ?>
+
+			<?php endif; ?>
+
+		</main>
+
+	</div>
+
+	<?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer(); ?>
