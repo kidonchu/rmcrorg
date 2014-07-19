8ee405d1b4184bb6797a9bb47b83954d9b4bed1f
kidonchu <kidonchu@gmail.com>
2014-07-19 00:29:15 -0500

    Add single-product.php

---
+++ wp-content/themes/rmcr/single-product.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Template Name Posts: Product
+ * The Template for displaying a product
+ *
+ * @package rmcr
+ */
+?>
+<?php get_header(); ?>
+	<div class="row">
+		<div class="col-md-9">
+
+			<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
+				yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+			} ?>
+
+			<main id="content" class="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php $images = get_images_src(); ?>
+
+					<?php foreach($images as $key => $image) : ?>
+						<img src="<?php echo $image[0]; ?>" alt=""/>
+					<?php endforeach; ?>
+
+
+					<?php echo do_shortcode( '[wp_cart_button name="' . get_the_title() .
+						'" price="' . get_field( 'price' ) . '" var1="' . str_replace( ',', '|', get_field( 'option1' ) ) . '"]' ); ?>
+
+				<?php endwhile; ?>
+
+			</main>
+
+			<div class="shopping-cart">
+				<?php // show shopping cart for every product page
+				echo do_shortcode( '[show_wp_shopping_cart]' );
+				?>
+			</div>
+
+		</div>
+
+		<?php get_sidebar(); ?>
+
+	</div>
+
+<?php get_footer(); ?>
